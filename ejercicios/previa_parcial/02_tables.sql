b937409b8a1216acab7b3ab8461db2fd896e5877
Francisco Andrade <403499@sistemas.frc.utn.edu.ar>
2026-06-17 21:42:50 -0300

    se agrego la solucion de previa_parcial, son ejercicios hecho en clases

--- ejercicios/previa_parcial/02_tables.sql
+++ ejercicios/previa_parcial/02_tables.sql
@@ -1,36 +1,40 @@
 USE GDA_PREVIA_PARCIAL;
 
 CREATE TABLE CLIENTES (
-    tipo_doc VARCHAR(10) CONSTRAINT clientes_tipo_doc_nn NOT NULL,
-    nro_doc  INT         CONSTRAINT clientes_nro_doc_nn  NOT NULL,
-    Apell    VARCHAR(50),
-    Nom      VARCHAR(50),
-    Fech_Nac DATE,
-    CONSTRAINT pk_clientes PRIMARY KEY (tipo_doc, nro_doc)
-);
+	tipo_doc VARCHAR(10),
+	nro_doc INT,
+	Apell VARCHAR(20) NOT NULL,
+	Nom VARCHAR(20) NOT NULL,
+	Fech_Nac DATE NOT NULL,
 
-CREATE TABLE VENTAS (
-    Nro_factura INT         CONSTRAINT ventas_nro_factura_nn NOT NULL,
-    Fecha       DATE,
-    tipo_doc    VARCHAR(10),
-    Nro_doc     INT,
-    CONSTRAINT pk_ventas PRIMARY KEY (Nro_factura),
-    CONSTRAINT fk_ventas_clientes FOREIGN KEY (tipo_doc, Nro_doc) REFERENCES CLIENTES (tipo_doc, nro_doc)
+	CONSTRAINT pk_clientes PRIMARY KEY (tipo_doc, nro_doc)
 );
 
 CREATE TABLE Articulos (
-    Cod    INT         CONSTRAINT articulos_cod_nn NOT NULL,
-    nombre VARCHAR(50),
-    precio DECIMAL(10, 2),
-    CONSTRAINT pk_articulos PRIMARY KEY (Cod)
+	Cod VARCHAR(10),
+	nombre VARCHAR(20) NOT NULL,
+	precio NUMERIC(10,2) NOT NULL CHECK (precio > 0),
+
+	CONSTRAINT pk_articulos PRIMARY KEY (Cod)
+);
+
+CREATE TABLE VENTAS (
+	Nro_factura INT,
+	Fecha DATETIME NOT NULL DEFAULT GETDATE(),
+	tipo_doc VARCHAR(10),
+	Nro_doc INT,
+	
+	CONSTRAINT pk_ventas PRIMARY KEY (Nro_factura),
+	CONSTRAINT fk_clientes FOREIGN KEY (tipo_doc, Nro_doc) REFERENCES CLIENTES (tipo_doc, nro_doc)
 );
 
 CREATE TABLE DETALLE (
-    Nro_Factura  INT            CONSTRAINT detalle_nro_factura_nn NOT NULL,
-    Cod_Articulo INT            CONSTRAINT detalle_cod_articulo_nn NOT NULL,
-    cant         INT,
-    precio       DECIMAL(10, 2),
-    CONSTRAINT pk_detalle PRIMARY KEY (Nro_Factura, Cod_Articulo),
-    CONSTRAINT fk_detalle_ventas FOREIGN KEY (Nro_Factura) REFERENCES VENTAS (Nro_factura),
-    CONSTRAINT fk_detalle_articulos FOREIGN KEY (Cod_Articulo) REFERENCES Articulos (Cod)
+	Nro_Factura INT,
+	Cod_Articulo VARCHAR(10),
+	cant INT NOT NULL CHECK (cant > 0),
+	precio NUMERIC(10,2) NOT NULL CHECK (precio > 0),
+
+	CONSTRAINT pk_detalle PRIMARY KEY (Nro_Factura, Cod_Articulo),
+	CONSTRAINT fk_ventas FOREIGN KEY (Nro_Factura) REFERENCES VENTAS (Nro_factura),
+	CONSTRAINT fk_articulos FOREIGN KEY (Cod_Articulo) REFERENCES Articulos (Cod)
 );
--- ejercicios/previa_parcial/consultas.sql
+++ ejercicios/previa_parcial/consultas.sql
@@ -2,20 +2,53 @@
 -- Resultado esperado:
 -- tipo_doc | nro_doc | Apell | Nom  | Fech_Nac
 -- DNI      | 1       | Perez | Juan | 1990-01-01
+SELECT *
+FROM CLIENTES c
+WHERE NOT EXISTS ( SELECT * 
+					FROM VENTAS v
+					WHERE v.Nro_doc = c.nro_doc
+				 )
 
 -- cual es la fecha de nacimiento del cliente mas viejo y su apellido
 -- Resultado esperado:
 -- Fech_Nac   | Apell
 -- 1980-05-15 | Gomez
+SELECT c.Fech_Nac, c.Apell
+FROM CLIENTES c
+WHERE c.Fech_Nac = ( SELECT MIN(Fech_Nac)
+					 FROM CLIENTES
+					);
+
 
 -- cual es el cliente con mas facturas o ventas
 -- Resultado esperado:
 -- tipo_doc | nro_doc | Apell | Nom    | cantidad_facturas
 -- DNI      | 3       | Lopez | Carlos | 6
+SELECT c.*, COUNT(1) as cant_ventas
+FROM CLIENTES c 
+JOIN VENTAS v ON c.nro_doc = v.Nro_doc AND c.tipo_doc = v.tipo_doc
+GROUP BY c.nro_doc, c.tipo_doc, c.Apell, c.Nom, c.Fech_Nac
+HAVING COUNT(1) >= ALL (SELECT COUNT(1)
+						FROM VENTAS v2
+						GROUP BY v2.Nro_doc, v2.tipo_doc
+						)
 
 -- cual es el cliente con mas facturas en cada mes del año actual
 -- Resultado esperado (Año 2026):
 -- Mes | tipo_doc | nro_doc | Apell | Nom    | cantidad_facturas
 -- 1   | DNI      | 3       | Lopez | Carlos | 2
 -- 2   | DNI      | 4       | Diaz  | Ana    | 2
--- 6   | DNI      | 3       | Lopez | Carlos | 3
+-- 6   | DNI      | 3       | Lopez | Carlos | 3
+
+--Agrupo por el mes y año (actual por el filtro en where). Esto se lo paso a la consulta (x_mes y 2026) para que filtre solo esos y cuente
+SELECT c.*, COUNT(1) as cant_facturas
+FROM CLIENTES c 
+JOIN VENTAS v on c.nro_doc = v.Nro_doc and c.tipo_doc = v.tipo_doc
+WHERE YEAR(GETDATE()) = YEAR(v.Fecha)
+GROUP BY c.tipo_doc, c.nro_doc, c.Fech_Nac, c.tipo_doc, c.Apell, c.Nom, YEAR(v.fecha), MONTH(v.Fecha)
+HAVING COUNT(1) >= ALL (SELECT COUNT(1)
+						FROM VENTAS v2
+						WHERE YEAR(v2.Fecha) = YEAR(v.Fecha)
+						AND MONTH(v2.Fecha) = MONTH(v.Fecha)
+						GROUP BY v2.Nro_doc, v2.tipo_doc
+						)
